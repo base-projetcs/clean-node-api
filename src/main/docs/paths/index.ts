4ee0337b52623b33a41191bebf42d125a3f994db
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-12-14 18:50:20 -0300

    style: linting

--- src/main/docs/paths/index.ts
+++ src/main/docs/paths/index.ts
@@ -1,6 +1,5 @@
 import { loginPath } from './login'
 import { signupPath } from './signup'
-
 export default {
   '/login': loginPath,
   '/signup': signupPath
--- src/main/docs/schemas/index.ts
+++ src/main/docs/schemas/index.ts
@@ -3,7 +3,6 @@ import { account } from './account'
 import { error } from './error'
 import { loginParams } from './login-params'
 import { signUpParams } from './signup-params'
-
 export default {
   error,
   apiKeyAuth,
